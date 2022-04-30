d6f841e009ea667f59fb72103c286c4eabd266e6
Antonio Sarosi <sarosiantonio@gmail.com>
2022-04-30 19:58:42 +0200

    Ejercicio 3

--- routes/web.php
+++ routes/web.php
@@ -95,3 +95,27 @@ Route::post('/ejercicio2/c', function (Request $request) {
         'discount' => $discount,
     ]);
 });
+
+// Ejercicio 3
+
+Route::post('/ejercicio3', function (Request $request) {
+    $request->validate([
+        'name' => 'required|string|max:64',
+        'description' => 'required|string|max:512',
+        'price' => 'required|numeric|gt:0',
+        'has_battery' => 'required|boolean',
+        'battery_duration' => 'required_if:has_battery,true|numeric|gt:0',
+        'colors' => 'required|array',
+        'colors.*' => 'required|string',
+        'dimensions' => 'required|array',
+        'dimensions.width' => 'required|numeric|gt:0',
+        'dimensions.length' => 'required|numeric|gt:0',
+        'dimensions.height' => 'required|numeric|gt:0',
+        'accessories' => 'required|array',
+        'accessories.*' => 'required|array',
+        'accessories.*.name' => 'required|string',
+        'accessories.*.price' => 'required|numeric|gt:0',
+    ]);
+
+    return response();
+});
