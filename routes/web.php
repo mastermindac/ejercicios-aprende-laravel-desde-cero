145e495b4f59bde91827015929a8cfee66c22add
Antonio Sarosi <sarosiantonio@gmail.com>
2022-04-30 19:33:37 +0200

    Ejercicio 2

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,57 @@ Route::patch('/ejercicio1', function() {
 Route::delete('/ejercicio1', function() {
     return "DELETE OK";
 });
+
+// Ejercicio 2
+
+Route::post('/ejercicio2/a', function (Request $request) {
+    return Response::json([
+        'name' => $request->get('name'),
+        'description' => $request->get('description'),
+        'price' => $request->get('price'),
+    ]);
+});
+
+Route::post('/ejercicio2/b', function (Request $request) {
+    if ($request->get('price') < 0) {
+        return Response::json(['message' => "Price can't be less than 0"])->setStatusCode(422);
+    }
+
+    return Response::json([
+        'name' => $request->get('name'),
+        'description' => $request->get('description'),
+        'price' => $request->get('price'),
+    ]);
+});
+
+Route::post('/ejercicio2/c', function (Request $request) {
+
+    // Una forma simple para sacar el discount, con if-else
+    $discount = 0;
+    if ($request->query('discount') == "SAVE5") {
+        $discount = 5;
+    } else if ($request->query('discount') == "SAVE10") {
+        $discount = 10;
+    } else if ($request->query('discount') == "SAVE15") {
+        $discount = 15;
+    }
+
+    // Otra forma de sacar el discount, con el match de PHP 8
+    // $discount = match ($request->query('discount')) {
+    //     'SAVE5' => 5,
+    //     'SAVE10' => 10,
+    //     'SAVE15' => 15,
+    //     default => 0,
+    // };
+
+    // Calcular precio nuevo
+    $price = (100 - $discount) / 100 * $request->get('price');
+
+    // Devolver respuesta
+    return Response::json([
+        'name' => $request->get('name'),
+        'description' => $request->get('description'),
+        'price' => $price,
+        'discount' => $discount,
+    ]);
+});
