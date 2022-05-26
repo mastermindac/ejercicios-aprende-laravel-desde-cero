3a623e439634682297805883f9229c67ee2dad9c
JohnMDAW <johnmdaw@gmail.com>
2022-05-26 21:55:30 +0200

    Ejercicio 2 Error en C : Cannot unpack array with string keys

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,51 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Ejercicio 1
+// Ejercicio 2 A 
 
-Route::get('/ejercicio1', function () {
-    return "GET OK";
+Route::post('/ejercicio2/a', function (Request $request) {
+    return Response::json([
+        "name" => $request->name,
+        "description" => $request->description,
+        "price" => $request->price,
+    ]);
 });
 
-Route::post('/ejercicio1', function () {
-    return "POST OK";
+// Ejercicio 2 B
+
+Route::post('/ejercicio2/b', function (Request $request) {
+    $price = $request->price;
+    $mensaje = "";
+    if($price < 0){
+        $mensaje = "Price can't be less than 0";
+    }else{
+        $mensaje = "The price is " . $price;
+    }
+    return Response::json([
+        "message"=> $mensaje
+    ])->setStatusCode(422);
+});
+
+
+// Ejercicio 2 C
+
+route::post('/ejercicio2/c', function (Request $request){
+    if(!empty($request->get("discount"))){
+        $discountCode = $request->get("discount"); 
+        $discount = intval(trim($discountCode, "SAVE"));
+    }else{
+        $discount = 0;
+    }
+    $price = $request->price;
+    if($discount > 0){
+        $price = $price - (($price * $discount)/100)  ; 
+    }else{
+        $price;
+    }
+    return Response::json([
+        "name" => $request->name,
+        "description" => $request-> description,
+        "price" => $price,
+        "discount" => $discount 
+    ]);
 });
