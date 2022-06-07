07893e756c74683d8b95b90c38b57e838fcf2919
JohnMDAW <johnmdaw@gmail.com>
2022-06-07 21:16:15 +0200

    Solucion ejercicio #3

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::post('ejercicio3', function (Request $req) {
+    $validated = $req->validate([
+    'name' => ['required', 'string','max:64'],
+    'description' => ['required', 'string', 'max:512'],
+    'price' => ['required', 'numeric', 'gt:0'],
+    'has_battery' => ['required', 'boolean'],
+    'battery_duration' => ['required_if:has_battery,true','gt:0','numeric'],
+    'dimensions' => ['required', 'array'],
+    'dimensions.width' => ['required', 'numeric', 'gt:0'],
+    'dimensions.length' => ['required', 'numeric', 'gt:0'],
+    'dimensions.height' => ['required', 'numeric', 'gt:0'],
+    'colors' => ['required', 'array'],
+    'colors.*' => ['required', 'string'],
+    'accessories' => ['required', 'array'],
+    'accessories.*' => ['required', 'array'],
+    'accessories.*.name' => ['required', 'string'],
+    'accessories.*.price' => ['required', 'numeric', 'gt:0']
+    ]);
+    return response();
 });
 
-// Ejercicio 1
 
-Route::get('/ejercicio1', function () {
-    return "GET OK";
-});
-
-Route::post('/ejercicio1', function () {
-    return "POST OK";
-});
+// $validated = $req->validate([
+//     'name' => ['required|string|max:64'],
+//     'description' => 'required|string|max:512',
+//     'price' => 'required|numeric|gt:0',
+//     'has_battery  ' => 'required|boolean',
+//     'battery_duration' => ['required_if:has_battery,true|gt:0|numeric'],
+//     'dimensions' => 'required|array',
+//     'dimensions.width' => 'required|numeric|gt:0',
+//     'dimensions.length' => 'required|numeric|gt:0',
+//     'dimensions.height' => 'required|numeric|gt:0',
+//     'colors' => 'required|array',
+//     'colors.*' => 'required|string',
+//     'accessories' => 'required|array',
+//     'accessories.*' => 'required|array',
+//     'accessories.*.name' => 'required|string',
+//     'accessories.*.price' => 'required|numeric|gt:0'
+// ]);
+// return $validated;
