faa48ffaf406bbc84cc6708be728eab798614ada
Juan Nuñez <jsn1096@gmail.com>
2022-08-15 22:17:48 -0500

    falta una

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use Illuminate\Http\Client\Request as ClientRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
@@ -29,3 +30,45 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+
+Route::post('/ejercicio2/a', function () {
+    return Response::json(["name"=>"Keyboard",
+    "description"=>"Mechanical RGB keyboard",
+    "price"=>200]);
+});
+
+Route::post('/ejercicio2/b', function () {
+    $product = ["name"=>"Keyboard",
+    "description"=>"Mechanical RGB keyboard",
+    "price"=>-100];
+
+    if ($product["price"] < 0) {
+        return Response::json(["message"=>"Price can't be less than 0"])->setStatusCode(422);
+    }
+});
+
+Route::post('/ejercicio2/c?discount=SAVE5', function (Request $request) {
+
+    if ($request->query('discount') === "SAVE5") {
+        return Response::json(["name"=>"Keyboard",
+        "description"=>"Mechanical RGB keyboard",
+        "price"=>190,
+        "discount"=>5]);
+    } elseif ($request->query('discount') === "SAVE10") {
+        return Response::json(["name"=>"Keyboard",
+        "description"=>"Mechanical RGB keyboard",
+        "price"=>180,
+        "discount"=>10]);
+    } elseif ($request->query('discount') === "SAVE15") {
+        return Response::json(["name"=>"Keyboard",
+        "description"=>"Mechanical RGB keyboard",
+        "price"=>170,
+        "discount"=>15]);
+    } else {
+        return Response::json(["name"=>"Keyboard",
+        "description"=>"Mechanical RGB keyboard",
+        "price"=>200,
+        "discount"=>0]);
+    }
+});
+
