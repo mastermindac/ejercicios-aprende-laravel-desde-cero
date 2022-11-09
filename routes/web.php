84a1bdf29f0c2f34e9d1f2ad6ab767f1d30711dc
Alberto <albertortego@gmail.com>
2022-11-09 11:02:55 +0100

    rutas resource de laravel

--- routes/web.php
+++ routes/web.php
@@ -63,10 +63,12 @@ Route::post('/contact', function(Request $request) {
 
 });
 
-Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
-Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
-Route::put('/contacts/{contact}/edit', [ContactController::class, 'update'])->name('contacts.update');
-Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
-Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+// Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+// Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+// Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
+// Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+// Route::put('/contacts/{contact}/edit', [ContactController::class, 'update'])->name('contacts.update');
+// Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+// Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+
+Route::resource('contacts', ContactController::class);
