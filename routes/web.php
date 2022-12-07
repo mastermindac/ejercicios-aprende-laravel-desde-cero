6f7733ac4da28801074bb811fc7bf1510fc68517
Alvaro Almira <alvaroalmira@hotmail.es>
2022-12-07 14:58:05 +0100

    Solucion 2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,54 @@ Route::get('/ejercicio1', function () {
     return "GET OK";
 });
 
+
 Route::post('/ejercicio1', function () {
     return "POST OK";
+});
+
+
+Route::post('/ejercicio2/a', function(Request $request) {
+    return Response::json([
+        'name'=>$request->get('name'),
+        'description'=>$request->get('description'),
+        'price'=>$request->get('price')
+    ]);
+});
+
+
+Route::post('/ejercicio2/b', function (Request $request) {
+    if ($request->get('price') < 0) {
+        return Response::json(['message' => "Price can't be less than 0"])->setStatusCode(422);
+    }
+    
+    return Response::json([
+        'name' => $request->get('name'),
+        'description' => $request->get('description'),
+        'price' => $request->get('price'),
+    ]);
+});
+
+
+Route::post('ejercicio2/c', function (Request $request) {
+    $descuento = $request->query('discount');
+
+    $valor_descuento = 0;
+
+    if($descuento == "SAVE5") {
+        $valor_descuento = 5;
+    } else if($descuento == "SAVE10") {
+        $valor_descuento = 10;
+    } else if($descuento == "SAVE15") {
+        $valor_descuento = 15;
+    }
+
+    $precio_original = $request->get('price');
+    $precio_con_descuento = $precio_original * ((100 - $valor_descuento) / 100);
+
+    return Response::json([
+        'name' => $request->get('name'),
+        'description' => $request->get('description'),
+        'price' => $precio_con_descuento,
+        "discount" => $valor_descuento,
+    ]);
 });
