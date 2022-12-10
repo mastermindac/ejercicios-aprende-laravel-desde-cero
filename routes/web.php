6e5e2e2e96c032db69e0fdc894fd2e64045055b8
Álvaro Ramiro Beanvides <alraben@cgcom.es>
2022-12-11 00:30:24 +0100

    Solución Ejercicio 2

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,44 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+
+
+Route::post('/ejercicio2/a', function (Request $request) {
+    return response($request, 200);
+});
+
+
+
+Route::post('/ejercicio2/b', function (Request $request) {
+
+    $price = $request->get('price');
+
+    if ($price < 0)
+        return response()->json(['message' => 'Price can\'t be less than 0'], 422);
+    else
+        return $price;
+});
+
+
+Route::post('/ejercicio2/c', function (Request $request) {
+
+    $discount = $request->query('discount');
+    $price = $request->get('price');
+    $name = $request->get('name');
+    $description = $request->get('description');
+
+    if ($discount == "SAVE5") {
+        $price = $price - $price * 0.05;
+        return response()->json(['name' => $name, 'description' => $description, 'price'=>$price,'discount'=>5], 200);
+
+    } elseif ($discount == "SAVE10") {
+        $price = $price - $price * 0.10;
+        return response()->json(['name' => $name, 'description' => $description, 'price'=>$price,'discount'=>10], 200);
+    } elseif ($discount == "SAVE15") {
+        $price = $price - $price * 0.15  ;
+        return response()->json(['name' => $name, 'description' => $description, 'price'=>$price,'discount'=>15], 200);
+    } else
+        return response()->json(['name' => $name, 'description' => $description, 'price'=>$price,'discount'=>0], 200);
+
+
+    });
