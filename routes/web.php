bdf1ba638366200a5dff80de1ba9151afe5e6b21
jesus5555 <rezajesusfrancisco@gmail.com>
2022-12-16 16:37:32 -0700

    git add.

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,22 @@ Route::get('/', function () {
 
 // Ejercicio 1
 
-Route::get('/ejercicio1', function () {
+Route::get('/ejercicio1', function (){
     return "GET OK";
 });
 
-Route::post('/ejercicio1', function () {
+Route::post('/ejercicio1', function (){
     return "POST OK";
+});
+
+Route::put('/ejercicio1', function(){
+    return "PUT OK";
+});
+
+Route::patch('/ejercicio1', function(){
+    return "PATCH OK";
+});
+
+Route::delete('/ejercicio1', function(){
+    return "DELETE OK";
 });
