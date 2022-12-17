96b03cad12ef845dcbbe7ffcbaed00ebee4745d9
Álvaro Ramiro Beanvides <alraben@cgcom.es>
2022-12-18 00:17:51 +0100

    Solucion 3

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,43 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+
+
+Route::post('/ejercicio3', function (Request $request) {
+
+
+    //$json = json_decode($request->getContent());
+
+    $request->validate([
+        'name' => 'required|max:64',
+        'description' => 'required|max:512',
+        'price' => 'required|numeric|gt:0',
+        'has_battery' => 'required|boolean',
+        'battery_duration' => 'required_if:has_battery,true|numeric|min:0',
+        'colors' => 'required',
+        'colors.*' => 'string',
+        'dimensions' => 'required',
+        'dimensions.width' => 'numeric|gt:0',
+        'dimensions.height' => 'numeric|gt:0',
+        'dimensions.length' => 'numeric|gt:0',
+        'accessories' => 'required||array',
+        'accessories.*.name' => 'required|string',
+        'accessories.*.price' => 'required|numeric|gt:0',
+    ]);
+
+   
+
+
+    /*
+    $request->validate([
+        'name' => 'required',
+        'phone_number' => ['required','digits:9'],
+        'email' => ['required','email'],
+        'age' => ['required','numeric','min:1','max:255']
+    ]);
+*/
+
+
+    return response('');
+
+});
