2036baa6279f4f703bbedc83c46d55bee1b047f1
frany6710 <franciscoglogauer@gmail.com>
2022-12-25 01:24:58 -0300

    Ejercicio resuelto

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request as FacadesRequest;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +29,25 @@ Route::get('/ejercicio1', function () {
 
 Route::post('/ejercicio1', function () {
     return "POST OK";
+});
+
+Route::post('/ejercicio3', function(Request $req) {
+    $req->validate([
+        'name' => 'required|string|max:64',
+        'description' => 'required|string|max:512',
+        'has_battery' => 'required|boolean',
+        'battery_duration' => 'required_if:has_battery,true|numeric|gt:0',
+        'price' => 'numeric|required|gt:0',
+        'colors' => 'array|required',
+        'colors.*' => 'string|required',
+        'dimensions' => 'required|array',
+        'dimensions.width' => 'integer|required|gt:0',
+        'dimensions.height' => 'integer|required|gt:0',
+        'dimensions.length' => 'integer|required|gt:0',
+        'accessories' => 'required|array',
+        'accessories.*' => 'required|array',
+        'accessories.*.name' => 'string|required',
+        'accessories.*.price' => 'required|numeric|gt:0'
+
+    ]);
 });
