092a160fca9614c17fa1d43842329881d7737230
aldearco <albertodelarco@outlook.com>
2023-01-18 21:04:28 +0100

    Solución 4

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'user_id',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
