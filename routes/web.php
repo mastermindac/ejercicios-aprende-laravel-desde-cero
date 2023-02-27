3b66a1dfc267a582873a4555b4cd185f32e83de6
Fran <fran69s@hotmail.es>
2023-02-27 14:12:50 +0100

    Solucion Ejercicio 2

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,47 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+
+
+Route::post('/ejercicio2/a', function (Request $request) {
+    return $request;
+});
+
+Route::post('/ejercicio2/b', function (Request $request) {
+    $precio = $request->get("price");
+
+    if($precio < 0){
+        return Response::json(["message"=>"Price can't be less than 0"])->setStatusCode(422);
+    }else{
+        return Response::json(["message"=>"Price correct"]);
+    }
+    
+});
+
+Route::post('/ejercicio2/c', function (Request $request) {
+    $descuento = $request->get("discount");
+    $precio = $request->get("price");
+
+    
+     if($descuento != NULL){
+        if($descuento == "SAVE5"){
+            $cantidad = 0.05;
+            $precio_final = $precio - ($precio*$cantidad);
+
+            return Response::json(["name"=>$request->get("name"),"description"=>$request->get("description"),"price"=>$precio_final,"discount"=>5]);
+        }elseif($descuento == "SAVE10"){
+            $cantidad = 0.1;
+            $precio_final = $precio - ($precio*$cantidad);
+
+            return Response::json(["name"=>$request->get("name"),"description"=>$request->get("description"),"price"=>$precio_final,"discount"=>10]);
+        }else{
+            $cantidad = 0.15;
+            $precio_final = $precio - ($precio*$cantidad);
+
+            return Response::json(["name"=>$request->get("name"),"description"=>$request->get("description"),"price"=>$precio_final,"discount"=>15]);
+        }
+    }else{
+        return Response::json(["name"=>$request->get("name"),"description"=>$request->get("description"),"price"=>$request->get("price"),"discount"=>0]);
+    } 
+    
+});
