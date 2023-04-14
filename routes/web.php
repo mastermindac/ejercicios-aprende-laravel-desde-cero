8c970954db3a5e20a43c9cf55a47ae1b91646f9a
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2023-04-14 12:08:59 +0200

    Solucion 1

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,15 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+
+Route::put('/ejercicio1', function () {
+    return "PUT OK";
+});
+
+Route::delete('/ejercicio1', function () {
+    return "DELETE OK";
+});
+
+Route::patch('/ejercicio1', function () {
+    return "PATCH OK";
+});
