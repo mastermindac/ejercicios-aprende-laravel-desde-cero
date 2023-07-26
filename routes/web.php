f4366438ec2a4844e1348be5b44f2ef51e6ad628
Mmorales8 <miguel.moralestorregrosa@gmail.com>
2023-07-26 21:15:24 +0200

    Solucion ejercicio 3

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,25 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+
+
+// Ejercicio 3
+
+Route::post('/ejercicio3', function(Request $request) {
+    $request -> validate([
+        'name' => 'required|string|max:64',
+        'description' => 'required|string|max:512',
+        'price' => 'required|numeric|gt:0',
+        'has_battery' => 'required|boolean',
+        'battery_duration' => 'required_if:has_battery,true|numeric|gt:0',
+        'colors' => 'required',
+        'colors.*' => 'string',
+        'dimensions' => 'required',
+        'dimensions.width' => 'required|numeric|gt:0',
+        'dimensions.height' => 'required|numeric|gt:0',
+        'dimensions.length' => 'required|numeric|gt:0',
+        'accessories' => 'required',
+        'accessories.*.name' => 'required|string',
+        'accessories.*.price' => 'required|numeric|gt:0',
+    ]);
+});
