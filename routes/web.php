16241c80c9e6955fa74eeea60fc547302b696835
cesar <norsciacesar@gmail.com>
2023-08-01 10:41:39 -0400

    nada

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,17 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+
+Route::post('/ejercicio3' , function(Request $request) {
+    $request->validate([
+        'name' => ['required', 'string','max:64'],
+        'description' => ['required','string','max:512'],
+        'price' => ['required', 'numeric','gt:0'],
+        'has_battery' => ['required', 'boolean'],
+        'battery_duration' => ['required_if:has_battry,true' ,'numeric', 'gt:0'],
+        'colors' => ['required' , 'array:string' ],
+        'dimensions' => ['required' , 'array:width','array:height', 'array:lenght' , 'gt:0'],
+        'accessories' => ['required' , 'array:array', 'gt:0']
+        
+    ]);
+});
