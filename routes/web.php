2c4d55b970f8f8669d32201fac9a6d10eb2c3a9a
cesar <norsciacesar@gmail.com>
2023-08-01 10:20:46 -0400

    intento fallido

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\Rule;
 
 /*
 |--------------------------------------------------------------------------
