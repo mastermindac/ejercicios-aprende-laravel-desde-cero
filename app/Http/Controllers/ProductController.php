f19b3d3cee8ddc3bc2be9370eab98087a90b4308
cesar <norsciacesar@gmail.com>
2023-08-02 19:49:00 -0400

    primer intento

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProductRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = auth()->user()->products;
+        return Response::json(["product" => $products]);
+    }
+    
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProductRequest $request)
+    {
+        auth()->user()->products()->create($request->validate());
+
+        return Response::json([
+            'message' => "Product created successfully",
+            "product" => [
+                'id' => $request->id(),
+                'name' => $request->name(),
+                'description' => $request->description(),
+                'price' => $request->price(),
+                "created_at" => $request->created_at(),
+                "updated_at" => $request->updated_at(),
+            ],
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return Response::json(["product" => $product]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StoreProductRequest $request ,Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreProductRequest $request, Product $product)
+    {
+        $this->authorize('update', $product);
+
+        $product->update($request->validated());
+
+        return Response::json([
+            "message" => "Product updated successfully",
+            "product" => $product
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $this->authorize('delete', $product);
+
+        $product->delete();
+
+        return Response::json([
+            "message" => "Product deleted successfully",
+            "product" => $product
+        ]);
+    }
+}
